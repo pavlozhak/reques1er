c120e7e77bd01e0089052b3e49221caf3d0463e4
StrangeDev <pavlozhak@gmail.com>
2019-08-05 19:32:44 +0300

    Create base file

---
+++ reques1er.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "================================\n\r";
+echo "\n\r";
+echo "PHP CURL REQUESTER v0.1\r\n";
+echo "\n\r";
+echo "================================\n\r";
+echo "\n\r";
+
+$counter = $argv[1];
+
+echo "Your argument is: ".$counter."\n\r";
+echo "Enjoy!\n\r";
+
+?>
