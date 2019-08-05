77cd70efcdba11ddc789ce9f4847b24485e4911e
StrangeDev <pavlozhak@gmail.com>
2019-08-05 20:23:40 +0300

    Add options

--- reques1er.php
+++ reques1er.php
@@ -1,7 +1,5 @@
 <?php
 
-$site = "https://staging.strange-dev.com/";
-
 echo "================================\n\r";
 echo "\n\r";
 echo "     PHP CURL REQUESTER v0.1\r\n";
@@ -9,20 +7,15 @@ echo "\n\r";
 echo "================================\n\r";
 echo "\n\r";
 
-if(empty($argv[1])) {
-    echo "ERROR! Counter is empty\n\r";
-    echo "Example: php {$argv[0]} 13\n\r";
-    echo "\n\r";
-} else {
-    $counter = $argv[1];
+$url = getopt(null, ["url:"]);
+$counter = getopt(null, ["counter:"]);
 
-    if(!is_numeric($counter)) { echo "ERROR! The argument must be a number\n\r"; exit();}
+var_dump($counter);
 
+function sendRequests($url, $counter) {
     $mh = curl_multi_init();
 
-    for($i = 0; $i < $counter; $i++) {
-
-        $url = $site;
+    for($i = 0; $i < $counter['counter']; $i++) {
         $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
 
         ${'ch_'.$i} = curl_init();
@@ -34,7 +27,7 @@ if(empty($argv[1])) {
         curl_setopt(${'ch_'.$i}, CURLOPT_VERBOSE, true);
         curl_setopt(${'ch_'.$i}, CURLOPT_RETURNTRANSFER, true);
         curl_setopt(${'ch_'.$i}, CURLOPT_USERAGENT, $agent);
-        curl_setopt(${'ch_'.$i}, CURLOPT_URL,$url);
+        curl_setopt(${'ch_'.$i}, CURLOPT_URL,$url['url']);
 
         curl_multi_add_handle($mh, ${'ch_'.$i});
     }
@@ -46,12 +39,8 @@ if(empty($argv[1])) {
         }
     } while ($active && $status == CURLM_OK);
 
-
-    //echo "Your argument is: ".$counter."\n\r";
-    echo "Enjoy!\n\r";
-    echo "\n\r";
+    sendRequests($url, $counter);
 }
 
-
-
+sendRequests($url, $counter);
 ?>
